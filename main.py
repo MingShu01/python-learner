c60a527b96b903ba14e948ebd5fca0d81ce89d8f
Ming Shu <jabinshu@163.com>
2024-07-16 17:19:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
+import datetime, request
+
 def main():
-  
+  Nowtime = datetime.datetime()
+  response = request.get('https://www.github.com')
+  time = datetime.datetime()
+  return response.status_code time
+
+if __name__ = main
