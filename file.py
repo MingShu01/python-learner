85cd1f0837686fdc2b6b355195944ab87688f298
Ming Shu <jabinshu@163.com>
2024-08-07 18:45:34 +0800

    Update file.py

--- file.py
+++ file.py
@@ -4,3 +4,10 @@ with pymysql.connect(**config) as connection:
     with connection.cursor() as cursor:
         cursor.execute("SELECT * FROM users")
         result = cursor.fetchall()
+
+
+import requests
+url = "https://www.bilibili.com/"
+html = requests.get(url=url)
+html = html.text
+print(html)
