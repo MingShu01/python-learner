5aa5fd833e0d031aed90292d6047d8bf0c63fd6a
Ming Shu <jabinshu@163.com>
2024-08-08 10:04:22 +0800

    Update file.py

--- file.py
+++ file.py
@@ -11,3 +11,11 @@ url = "https://www.bilibili.com/"
 html = requests.get(url=url)
 html = html.text
 print(html)
+
+
+import json
+
+data = [ { 'a' : 1, 'b' : 2, 'c' : 3, 'd' : 4, 'e' : 5 } ]
+
+data2 = json.dumps(data)
+print(data2)
