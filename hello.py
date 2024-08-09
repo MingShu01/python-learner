d7b06c4f7c8669d19c95fcca516221d91269fc36
Ming Shu <jabinshu@163.com>
2024-08-09 16:21:08 +0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -4,3 +4,14 @@ print('Argentina is champion')
 print('Messi is goat')
 print('This is a learn python course')
 print('Thank you very much!')
+
+
+import pygal
+
+radar_chart = pygal.Radar()
+radar_chart.title = '餐厅评分数据'
+radar_chart.x_labels = ['味道', '卫生', '服务', '价格', '环境']
+radar_chart.add('老王炸鸡', [9, 6, 6, 4, 7])
+radar_chart.add('小明快餐', [7, 8, 9, 6, 8])
+radar_chart.add('阿强烧烤', [10, 4, 6, 8, 4])
+radar_chart.add('萌仔汉堡', [7, 6, 5, 4, 6])
